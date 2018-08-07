36ec586644ffc4cde635aa206cfd47bb33b060e2
Julien Fontanet <julien.fontanet@isonoe.net>
2018-08-07 15:49:12 +0200

    chore(lint): add eslint-plugin-node
    And fix `new Buffer()` uses.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
   globals: {
     Promise: true,
   },
   parserOptions: {
     ecmaVersion: 5,
   },
+  plugins: ['node'],
   rules: {
     indent: 'off',
     'linebreak-style': ['error', 'unix'],
